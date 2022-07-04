7d0b13294b1be66fc80f7bad6db62266bfde1c74
Phillip F <93536176+Kurbit-Designs@users.noreply.github.com>
2022-07-04 02:05:45 +0100

    update

--- public_html/index.php
+++ public_html/index.php
@@ -1,10 +1,34 @@
-<?php 
-$title = "Homepage";
-$sitename = "Template-1";
-$sitelink = "template-1.co.uk";
-$desc =  "Desction about site goes here!";
-$keywords = "Add keywords here!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/homepage.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
 
-<?php include '../app/views/header.php';?>
-<?php include '../app/views/home.php';?>
+<div class="top-bar-contact">
+
+<div class="top-bar-contact-container">
+
+    <div class="phone-section">
+            <p>Call Us: <a href="tel:123-456-7890">01780 070707</a></p>
+    </div>
+
+    <div class="email-section">
+            <p>Email Us: <a href = "mailto: abc@example.com">Business@gmail.com</a></p>
+    </div>
+
+    <div class="social-section">
+         
+    </div>
+</div>
+</div>
+    
+</body>
+</html>
